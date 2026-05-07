82b5df9498f535ddae9c5ab137679f2f0ecd87bd
Abdul Fikri <fikriabdul679@gmail.com>
2026-05-07 21:00:01 +0700

    first commit

---
+++ data/whatsapp.js
@@ -0,0 +1,144 @@
+window.priceData_whatsapp = {
+  "wa-channels": {
+    min: 10,
+    data: [
+      {
+        id: "2822",
+        qty: "100 Followers",
+        warranty: "No Garansi",
+        price: "Rp 4.379",
+        Keterangan: "Location: Global",
+        status: "ready",
+      },
+      {
+        id: "2822",
+        qty: "250 Followers",
+        warranty: "No Garansi",
+        price: "Rp 10.606",
+        Keterangan: "Location: Global",
+        status: "ready",
+      },
+      {
+        id: "2822",
+        qty: "500 Followers",
+        warranty: "No Garansi",
+        price: "Rp 20.326",
+        Keterangan: "Location: Global",
+        status: "ready",
+      },
+      {
+        id: "2822",
+        qty: "750 Followers",
+        warranty: "No Garansi",
+        price: "Rp 30.449",
+        Keterangan: "Location: Global",
+        status: "ready",
+      },
+      {
+        id: "2822",
+        qty: "1000 Followers",
+        warranty: "No Garansi",
+        price: "Rp 38.149",
+        Keterangan: "Location: Global",
+        status: "ready",
+      },
+    ],
+  },
+
+  "channel-reactions": {
+    min: 10,
+    data: [
+      {
+        id: "9648 - 9670",
+        qty: "100 Reactions",
+        warranty: "No Garansi",
+        price: "Rp 2.001",
+        Keterangan:
+          "Pilih salah satu / mix: 👍❤️😂😲😥🙏🔥🏆🎉👏😎😡😮🖕💩👎💪💔🤮💸💀💵",
+        status: "ready",
+      },
+      {
+        id: "9648 - 9670",
+        qty: "250 Reactions",
+        warranty: "No Garansi",
+        price: "Rp 4.550",
+        Keterangan:
+          "Pilih salah satu / mix: 👍❤️😂😲😥🙏🔥🏆🎉👏😎😡😮🖕💩👎💪💔🤮💸💀💵",
+        status: "ready",
+      },
+      {
+        id: "9648 - 9670",
+        qty: "500 Reactions",
+        warranty: "No Garansi",
+        price: "Rp 9.100",
+        Keterangan:
+          "Pilih salah satu / mix: 👍❤️😂😲😥🙏🔥🏆🎉👏😎😡😮🖕💩👎💪💔🤮💸💀💵",
+        status: "ready",
+      },
+      {
+        id: "9648 - 9670",
+        qty: "750 Reactions",
+        warranty: "No Garansi",
+        price: "Rp 13.125",
+        Keterangan:
+          "Pilih salah satu / mix: 👍❤️😂😲😥🙏🔥🏆🎉👏😎😡😮🖕💩👎💪💔🤮💸💀💵",
+        status: "ready",
+      },
+      {
+        id: "9648 - 9670",
+        qty: "1000 Reactions",
+        warranty: "No Garansi",
+        price: "Rp 17.501",
+        Keterangan:
+          "Pilih salah satu / mix: 👍❤️😂😲😥🙏🔥🏆🎉👏😎😡😮🖕💩👎💪💔🤮💸💀💵",
+        status: "ready",
+      },
+    ],
+  },
+
+  "channel-polling": {
+    min: 10,
+    data: [
+      {
+        id: "4460",
+        qty: "Pilihan ke-1",
+        warranty: "No Garansi",
+        price: "Rp 199",
+        Keterangan: "Harga tertera /1 Vote",
+        status: "ready",
+      },
+      {
+        id: "4461",
+        qty: "Pilihan ke-2",
+        warranty: "No Garansi",
+        price: "Rp 199",
+        Keterangan: "Harga tertera /1 Vote",
+        status: "ready",
+      },
+      {
+        id: "4462",
+        qty: "Pilihan ke-3",
+        warranty: "No Garansi",
+        price: "Rp 199",
+        Keterangan: "Harga tertera /1 Vote",
+        status: "ready",
+      },
+      {
+        id: "4463",
+        qty: "Pilihan ke-4",
+        warranty: "No Garansi",
+        price: "Rp 199",
+        Keterangan: "Harga tertera /1 Vote",
+        status: "ready",
+      },
+      {
+        id: "4464",
+        qty: "Pilihan ke-5",
+        warranty: "No Garansi",
+        price: "Rp 199",
+        Keterangan: "Harga tertera /1 Vote",
+        status: "ready",
+      },
+    ],
+  },
+};
